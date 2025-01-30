f9ebe95882fb427b7fc8b29f1019078c6ebd7b27
Dilphi <has11092001@gmail.com>
2025-01-30 15:17:37 +0500

    1.0

--- app.py
+++ app.py
@@ -1,42 +1,83 @@
-# Перед началом работы с кодом нужно установить библиотеку Flet с помощью pip install flet. Если не получилось установить то нужно ввести команду в cmd для откытия в vs code Ctrl + Shift + ~ или же Ё на Windows и ввести venv\venv\activate либо venv/bin/activate
+import flet as ft  # Библиотека Flet для разработки GUI.
+import sqlite3
 
-
-import flet as ft # Библеотека Flet для разработки GUI.
-#Основная фукция
-def main(page: ft.Page): #Функция для создания приложения, а (параметр: ft.Page) включение подсказок
+def main(page: ft.Page):  # Функция для создания приложения
         
-    page.title = "" #Название приложения
-    #Тема приложения.
-    page.theme_mode = "dark" # Можно изменить на сетлую light
-    #Вертикальное расположение элементов.
-    page.vertical_alignment = ft.MainAxisAlignment.CENTER # Все элементы будут по центру
-    
-    # Вывод текста ft.Text('текст который нужно вывести', color='цвет'). Если не указывать цвет, то подберётся цвет изходя из выбранной темы
-    user_label = ft.Text('Info')
-    # Поле дял ввода 
-    user_input = ft.TextField(value="0", width=150, text_align=ft.TextAlign.CENTER) # value первоначальное значение, width для указания ширины, text_align=ft.TextAlign.CENTER расположение текста
-    
-    def get_info(e):
-        user_label.value = user_input.value
+    page.title = "DevLexicon"  # Название приложения
+    page.theme_mode = "dark"  # Можно изменить на светлую light
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER  # Все элементы будут по центру
+
+    # Исправленные параметры окна
+    page.window.width = 350
+    page.window.height = 400
+    page.window.resizable = False
+
+    # Определение полей ввода
+    student_name = ft.TextField(label='Имя', width=200, on_change=lambda e: validate())
+    student_surname = ft.TextField(label='Фамилия', width=200, on_change=lambda e: validate())
+    student_group = ft.TextField(label='Группа', width=150, on_change=lambda e: validate(), text_align=ft.TextAlign.CENTER)
+    btn_reg = ft.OutlinedButton(text='Начать', width=150, on_click=lambda e: register(), disabled=True)
+
+    # Функция для регистрации пользователя
+    def register():
+        db = sqlite3.connect('student.mab')
+        cur = db.cursor()
+        cur.execute("""
+            CREATE TABLE IF NOT EXISTS students(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL,
+                surname TEXT NOT NULL,
+                groub TEXT NOT NULL
+            )
+        """)
+        cur.execute(
+            "INSERT INTO students (name, surname, groub) VALUES (?, ?, ?)", 
+            (student_name.value, student_surname.value, student_group.value)
+        )
+
+        db.commit()
+        db.close()
+
+        # Очистка полей ввода после регистрации
+        student_name.value = ''
+        student_surname.value = ''
+        student_group.value = ''
+        btn_reg.text = 'Успешно!'
+        btn_reg.disabled = True  # Делаем кнопку неактивной после регистрации
+
         page.update()
-    
-    page.add( #Вывод объектов на окно
-        ft.Row( #Создание определённого ряда в приложении
-            [ #Для указания создания списка выводва элементов используются []
-                ft.IconButton(ft.Icons.PLAY_ARROW, on_click=get_info), # Элемент иконки которую можно нажать
-                ft.Icon(ft.Icons.PAUSE), # Просто иконки
-                ft.ElevatedButton(text='Нажми меня', on_click=get_info),
-                ft.OutlinedButton(text='Нажми меня', on_click=get_info)          
-            ],
-            alignment=ft.MainAxisAlignment.CENTER # Горизонтальное расположения элементов
-        ),
+
+    # Функция проверки заполненности полей
+    def validate():
+        if all([student_name.value, student_surname.value, student_group.value]):
+            btn_reg.disabled = False
+        else:
+            btn_reg.disabled = True
+        page.update()
+
+    # Функция смены темы
+    def change_theme(e):
+        page.theme_mode = 'light' if page.theme_mode == 'dark' else 'dark'
+        page.update()
+
+    # Добавление виджетов на страницу
+    page.add(
+        ft.Row([ft.IconButton(ft.Icons.SUNNY, on_click=change_theme)]),
         ft.Row(
-            [   
-                user_label,
-                user_input
+            [
+                ft.Column(
+                    [
+                        ft.Text('Начало тестирования'),
+                        student_name,
+                        student_surname,
+                        student_group,
+                        btn_reg
+                    ]
+                )
             ],
-            alignment=ft.MainAxisAlignment.CENTER
+            alignment=ft.MainAxisAlignment.CENTER  # Горизонтальное расположение элементов
         )
     )
+
 # Запуск функции при запуске приложения
-ft.app(target=main) # Можно из обычного приложения сделать веб-приложение, после main помтавить запятую и написать view=ft.AppView.WEB_BROWSER
+ft.app(target=main)  # Можно из обычного приложения сделать веб-приложение, добавив view=ft.AppView.WEB_BROWSER
